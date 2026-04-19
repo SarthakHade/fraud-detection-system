04fb4d48300bbd030655ef5aa53df04c7e8c3b5b
SarthakHade <sarthakhade07@gmail.com>
2026-04-19 10:30:55 +0530

    Update ui-core.js

--- js/ui-core.js
+++ js/ui-core.js
@@ -7,7 +7,7 @@ let mx=0,my=0,rx=0,ry=0;
 document.addEventListener('mousemove',e=>{mx=e.clientX;my=e.clientY;cursor.style.left=mx+'px';cursor.style.top=my+'px'});
 function animRing(){rx+=(mx-rx)*0.12;ry+=(my-ry)*0.12;ring.style.left=rx+'px';ring.style.top=ry+'px';requestAnimationFrame(animRing)}
 animRing();
-
+ 
 /* ============================================================
    Background Hex Canvas Animation
    ============================================================ */
